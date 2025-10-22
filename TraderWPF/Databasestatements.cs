b2e7f785be3ed4685dd1f3a3664e3afacf313a4f
FodorRobert <fodorr@kkszki.hu>
2025-10-22 08:07:43 +0200

    Hash is ready

--- TraderWPF/Databasestatements.cs
+++ TraderWPF/Databasestatements.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -89,6 +90,34 @@ namespace TraderWPF
             conn._connection.Close();
 
             return dt.DefaultView;
+
+        }
+
+        public string generateSalt()
+        {
+            byte[] salt = new byte[16];
+
+            using (var rnd = RandomNumberGenerator.Create())
+            {
+
+                rnd.GetBytes(salt);
+
+            }
+
+            return Convert.ToBase64String(salt);
+
+        }
+
+        public string ComputeHmacSha256(string password, string salt)
+        {
+
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(salt)))
+            {
+
+                byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(hash);
+
+            }
 
         }
 
